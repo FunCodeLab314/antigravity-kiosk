4c7c7fbd4decc7addfe30526bad230779c6036a9
Celinor314 <asnorsumdad1469@gmail.com>
2025-12-21 13:46:31 +0800

    bubububug

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,47 +1,55 @@
-allprojects {
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
 
-    repositories {
+android {
+    namespace = "com.example.new_proj"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
-        google()
+    compileOptions {
+        // --- FIX 1: Enable Core Library Desugaring ---
+        isCoreLibraryDesugaringEnabled = true
 
-        mavenCentral()
-
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.new_proj"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+        
+        // --- FIX 2: Enable MultiDex (Required for desugaring) ---
+        multiDexEnabled = true
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
 }
 
-
-
-val newBuildDir: Directory =
-
-    rootProject.layout.buildDirectory
-
-        .dir("../../build")
-
-        .get()
-
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-
-
-subprojects {
-
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-
+flutter {
+    source = "../.."
 }
 
-subprojects {
-
-    project.evaluationDependsOn(":app")
-
-}
-
-
-
-tasks.register<Delete>("clean") {
-
-    delete(rootProject.layout.buildDirectory)
-
+dependencies {
+    // --- FIX 3: Add the desugaring library ---
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
